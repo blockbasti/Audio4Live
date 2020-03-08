b1a83aa2994b3ccb83d3841cbc8040e9d8bb3ede
Bastian Block <blockbasti99@gmail.com>
2020-03-08 10:20:19 +0100

    Add navigation transitions

---
+++ src/app/animations.ts
@@ -0,0 +1,59 @@
+import { trigger, transition, style, query, animateChild, animate, group } from "@angular/animations";
+
+export const slideInAnimation =
+    trigger('routeAnimations', [
+        transition((fromState, toState) => {
+            let pages = ['LandingPage', 'BuchenPage', 'LeistungenPage', 'ProfilPage', 'ReferenzenPage'];
+            return pages.indexOf(fromState) < pages.indexOf(toState)
+        }, [
+            style({ position: 'relative' }),
+            query(':enter, :leave', [
+                style({
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%'
+                })
+            ]),
+            query(':enter', [
+                style({ left: '-100%' })
+            ]),
+            query(':leave', animateChild()),
+            group([
+                query(':leave', [
+                    animate('300ms .15s ease-out', style({ left: '100%' }))
+                ]),
+                query(':enter', [
+                    animate('300ms .15s ease-out', style({ left: '0%' }))
+                ])
+            ]),
+            query(':enter', animateChild()),
+        ]),
+        transition((fromState, toState) => {
+            let pages = ['LandingPage', 'BuchenPage', 'LeistungenPage', 'ProfilPage', 'ReferenzenPage'];
+            return pages.indexOf(fromState) > pages.indexOf(toState)
+        }, [
+            style({ position: 'relative' }),
+            query(':enter, :leave', [
+                style({
+                    position: 'absolute',
+                    top: 0,
+                    right: 0,
+                    width: '100%'
+                })
+            ]),
+            query(':enter', [
+                style({ right: '-100%' })
+            ]),
+            query(':leave', animateChild()),
+            group([
+                query(':leave', [
+                    animate('300ms .15s ease-out', style({ right: '100%' }))
+                ]),
+                query(':enter', [
+                    animate('300ms .15s ease-out', style({ right: '0%' }))
+                ])
+            ]),
+            query(':enter', animateChild()),
+        ])
+    ]);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { LeistungenComponent } from './leistungen/leistungen.component';
@@ -22,14 +24,15 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FontAwesomeModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      { path: '', component: LandingComponent },
-      { path: 'leistungen', component: LeistungenComponent },
-      { path: 'profil', component: ProfilComponent },
-      { path: 'buchen', component: BuchenComponent },
-      { path: 'referenzen', component: ReferenzenComponent },
+      { path: '', component: LandingComponent, data: {animation: 'LandingPage'} },
+      { path: 'leistungen', component: LeistungenComponent,data: {animation: 'LeistungenPage'} },
+      { path: 'profil', component: ProfilComponent, data: {animation: 'ProfilPage'} },
+      { path: 'buchen', component: BuchenComponent, data: {animation: 'BuchenPage'} },
+      { path: 'referenzen', component: ReferenzenComponent, data: {animation: 'ReferenzenPage'} },
     ])
   ],
   providers: [],
