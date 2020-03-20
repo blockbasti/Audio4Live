4bd09db934961a5d4f03140e9aa73275f9cd606a
Bastian Block <blockbasti99@gmail.com>
2020-03-20 13:28:49 +0100

    [WIP]Add basic calendar

--- src/app/buchen/buchen.component.ts
+++ src/app/buchen/buchen.component.ts
@@ -1,15 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterContentInit, AfterViewInit } from '@angular/core';
+
+import {
+  ChangeDetectionStrategy,
+  TemplateRef
+} from '@angular/core';
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours
+} from 'date-fns';
+import { Subject } from 'rxjs';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarMonthViewDay,
+  CalendarDateFormatter,
+  CalendarView,
+  DAYS_OF_WEEK
+} from 'angular-calendar';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
 
 @Component({
   selector: 'app-buchen',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './buchen.component.html',
-  styleUrls: ['./buchen.component.scss']
+  styleUrls: ['./buchen.component.scss'],
+  providers: [
+    {
+      provide: CalendarDateFormatter
+    }
+  ]
 })
-export class BuchenComponent implements OnInit {
+export class BuchenComponent implements OnInit, AfterViewInit {
+
+
+  locale: string = 'de';
+  view: CalendarView = CalendarView.Month;
+  CalendarView = CalendarView;
+  viewDate: Date = new Date();
+  minDate: Date = new Date();
+
+  activeDayIsOpen: boolean = false;
+  weekStartsOn: number = DAYS_OF_WEEK.MONDAY;
+  weekendDays: number[] = [DAYS_OF_WEEK.SATURDAY, DAYS_OF_WEEK.SUNDAY];
+
+  events: CalendarEvent[] = [
+    
+  ];
+
+  dateIsValid(date: Date): boolean {
+    return date > this.minDate;
+  }
+
+  beforeMonthViewRender({ body }: { body: CalendarMonthViewDay[] }): void {
+    body.forEach(day => {
+      if (!this.dateIsValid(day.date)) {
+        day.cssClass = 'cal-disabled';
+      }
+    });
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit():void {
+
   }
 
 }
