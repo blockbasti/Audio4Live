298e80aefd957abcfb9de6de336fe7343ae37180
Bastian Block <git@bblock.dev>
2023-12-04 17:13:19 +0100

    hide email

--- src/app/shared/impressum/impressum.component.ts
+++ src/app/shared/impressum/impressum.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import emailScramble from 'email-scramble';
 import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 
 @Component({
   selector: 'app-impressum',
   templateUrl: './impressum.component.html',
-  styleUrls: ['./impressum.component.scss']
+  styleUrls: ['./impressum.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class ImpressumComponent {
+  public showEmail = false;
+  public emailContent = '';
+
+  public decodeEmail() {
+    let email = emailScramble.decode('vasb@nhqvb9yvir.qr');
+    this.emailContent = 'E-Mail: <a href="mailto:' + email + '">' + email + '</a></br>';
+    this.showEmail = true;
+  }
+
   constructor(public readonly modalRef: MdbModalRef<ImpressumComponent>) {}
 }
